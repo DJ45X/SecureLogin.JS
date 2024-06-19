3e0d317033f3630666910905c1a829dba5c90ddb
Brandon [DJ45X] <citylife219@gmail.com>
2024-06-19 08:50:02 -0500

    update: main with refactored code

--- main.js
+++ main.js
@@ -8,6 +8,39 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+function showMenu() {
+    rl.question("Choose an option:\n[1] Register a new user\n[2] Login\n[3] Exit\n", (choice) => {
+        handleChoice(choice);
+    })
+}
+
+function handleChoice(choice) {
+    switch (choice) {
+        case "1":
+            rl.question("Enter username: ", (username) => {
+                rl.question("Enter password: ", async (password) => {
+                    await registerUser(username, password);
+                    showMenu();
+                });
+            });
+            break;
+        case "2":
+            rl.question("Enter username: ", (username) => {
+                rl.question("Enter password: ", async (password) => {
+                    await loginUser(username, password);
+                    showMenu();
+                });
+            });
+            break;
+        case "3":
+            rl.close();
+            break;
+        default:
+            console.log("Invalid choice. Please try again.");
+            main();
+    };
+}
+
 async function main() {
     const dbInitialized = await initializeDB();
     if(!dbInitialized) {
@@ -15,32 +48,7 @@ async function main() {
         process.exit(1);
     }
 
-    rl.question("Choose an option:\n[1] Register a new user\n[2] Login\n[3] Exit\n", (choice) => {
-        switch (choice) {
-            case "1":
-                rl.question("Enter username: ", (username) => {
-                    rl.question("Enter password: ", async (password) => {
-                        await registerUser(username, password);
-                        rl.close();
-                    });
-                });
-                break;
-            case "2":
-                rl.question("Enter username: ", (username) => {
-                    rl.question("Enter password: ", async (password) => {
-                        await loginUser(username, password);
-                        rl.close();
-                    });
-                });
-                break;
-            case "3":
-                rl.close();
-                break;
-            default:
-                console.log("Invalid choice. Please try again.");
-                main();
-        };
-    });
+    showMenu();
 }
 
 
